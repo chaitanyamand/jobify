9eda002bd19ad0579d6be94e5030330f8a269630
Chaitanya <chaitanyam187@gmail.com>
2023-11-11 20:04:33 +0530

    Implement Fetch Location Using API

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,6 +3,7 @@ import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Logo, FormRow, SubmitBtn } from "../components";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
@@ -18,6 +19,36 @@ export const action = async ({ request }) => {
 };
 
 const Register = () => {
+  const fetchLocation = () => {
+    const fetchBtn = document.querySelector("#fetchLoc");
+    fetchBtn.disabled = true;
+    fetchBtn.style.opacity = 0.5;
+    fetchBtn.innerHTML = "Fetching Your Location";
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const { data } = await axios.get(
+          `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+        );
+        if (data.city && data.principalSubdivision && data.countryName) {
+          document.querySelector("#location").defaultValue =
+            data.city +
+            ", " +
+            data.principalSubdivision +
+            ", " +
+            data.countryName;
+          fetchBtn.innerHTML = "Fetched Your Location";
+        }
+      },
+      (error) => {
+        fetchBtn.innerHTML = "Fetching Location Failed!";
+        toast.error("fetching location failed");
+        console.log(error);
+      }
+    );
+  };
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -30,6 +61,15 @@ const Register = () => {
           defaultValue=""
         ></FormRow>
         <FormRow type="text" name="location" defaultValue=""></FormRow>
+        <button
+          type="button"
+          id="fetchLoc"
+          className="btn form-row"
+          style={{ marginTop: "0" }}
+          onClick={fetchLocation}
+        >
+          Fetch My Location
+        </button>
         <FormRow type="email" name="email" defaultValue=""></FormRow>
         <FormRow
           type="password"
